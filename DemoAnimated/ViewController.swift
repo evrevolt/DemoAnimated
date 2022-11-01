1cb30e3c5377aba4bbb8bd688e28f41964efc248
evrevolt <gennadi_wow@mail.ru>
2022-11-01 16:45:38 +0300

    Initial Commit

---
+++ DemoAnimated/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DemoAnimated
+//
+//  Created by Геннадий Ведерников on 01.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
